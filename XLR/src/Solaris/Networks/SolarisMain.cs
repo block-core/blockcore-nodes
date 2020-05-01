67fa1cd79fd4a15dd12005147a4dc98a176d0120
dangershony <dan.gershony@gmail.com>
2020-05-01 14:37:01 +0100

    Fix XLR bug

--- XLR/src/Solaris/Networks/SolarisMain.cs
+++ XLR/src/Solaris/Networks/SolarisMain.cs
@@ -121,7 +121,7 @@ namespace Solaris.Networks
          Base58Prefixes[(int)Base58Type.COLORED_ADDRESS] = new byte[] { 0x13 };
 
          Bech32Encoders = new Bech32Encoder[2];
-         var encoder = new Bech32Encoder(SolarisSetup.Main.CoinTicker);
+         var encoder = new Bech32Encoder(SolarisSetup.Main.CoinTicker.ToLowerInvariant());
          Bech32Encoders[(int)Bech32Type.WITNESS_PUBKEY_ADDRESS] = encoder;
          Bech32Encoders[(int)Bech32Type.WITNESS_SCRIPT_ADDRESS] = encoder;
 
