ae567c3c3850b678476f5fdd5a1f544807f77ab3
dangershony <dan.gershony@gmail.com>
2022-02-24 14:05:37 +0000

    Add amount and gas price

--- CRS/Cirrus.Node/Models/ContractReceiptResponse.cs
+++ CRS/Cirrus.Node/Models/ContractReceiptResponse.cs
@@ -14,11 +14,15 @@ namespace Cirrus.Node.Models
         public string ContractBytecode { get; set; }
         public string ContractCodeHash { get; set; }
         public string ContractCSharp { get; set; }
+        public ulong GasPrice { get; set; }
+        public ulong Amount { get; set; }
 
         public ContractReceiptResponse(Receipt receipt, List<LogResponse> logs, Network network)
             : base(receipt, logs, network)
         {
             this.MethodName = receipt.MethodName;
+            this.GasPrice = receipt.GasPrice;
+            this.Amount = receipt.Amount;
         }
     }
 }
